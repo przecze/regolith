5a5a84469df77d417415f60176d60b98ab934153
Przemyslaw Czechowski <przemyslaw.czechowski@cognostics.de>
2020-08-22 10:19:04 +0200

    Initial commit

---
+++ utils.h
@@ -0,0 +1,15 @@
+#include "btBulletDynamicsCommon.h"
+#include "LinearMath/btVector3.h"
+#include "LinearMath/btAlignedObjectArray.h"
+namespace utils {
+  btCompoundShape* BuildTowerCompoundShape(btVector3&& brickFullDimensions=
+                                                                btVector3(4.0,3.0,2.0),
+                                                  unsigned int numRows=10,
+                                                  unsigned int numBricksPerRow=10,
+                                                  bool useConvexHullShape=true);
+  static double sizeInverseDistribution(double p, double minRadius, double maxRadius) {
+    assert(p <=1.);
+    assert(p >=0.);
+    return maxRadius * std::exp( std::log(minRadius / maxRadius) * p);
+  }
+} // namespace utils
