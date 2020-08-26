38020e939cb261323f57c38975f0bae10216a9f2
Przemyslaw Czechowski <przemyslaw.czechowski@cognostics.de>
2020-08-26 21:54:32 +0200

    Use yaml library PoC

--- simulations/common/main.cpp
+++ simulations/common/main.cpp
@@ -7,10 +7,17 @@
 #include "LinearMath/btTransform.h"
 #include "LinearMath/btHashMap.h"
 
+#include "yaml-cpp/yaml.h"
+
+#include<iostream>
+
 CommonExampleInterface* CreateFunc(struct CommonExampleOptions& options);
 
 int main(int argc, char* argv[])
 {
+	YAML::Node config = YAML::LoadFile("config.yaml");
+	std::cout<<config["thing"]<<std::endl;
+
 	DummyGUIHelper noGfx;
 
 	CommonExampleOptions options(&noGfx);
